a63936818f63f209a6a06892b7fb3425bc08e941
Emily Nguyen (Antony) <emilyhuongnguyen@gmail.com>
2018-06-08 18:02:59 -0700

    Complete Fizz Buzz in Python3

---
+++ easy/Fizz Buzz/fizz-buzz.py
@@ -0,0 +1,27 @@
+"""
+Problem:
+
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number 
+and for the multiples of five output “Buzz”. For numbers which are multiples 
+of both three and five output “FizzBuzz”.
+"""
+
+class Solution:
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        output = []
+        for i in range(1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                output.append("FizzBuzz")
+            elif i % 3 == 0:
+                output.append("Fizz")
+            elif i % 5 == 0:
+                output.append("Buzz")
+            else:
+                output.append(str(i))
+        return output
