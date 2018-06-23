89925d7b8b815e1017abc25f8c475715e23d5de9
Emily Nguyen (Antony) <emilyhuongnguyen@gmail.com>
2018-06-22 23:28:27 -0700

    Complete Swap Salary in MySQL

---
+++ easy/Swap Salary/swap-salary.sql
@@ -0,0 +1,15 @@
+/* 
+ * Problem:
+ *
+ * Given a table salary, such as the one below, that has m=male and f=female values. 
+ * Swap all f and m values (i.e., change all f values to m and vice versa) with a single 
+ * update query and no intermediate temp table.
+ */
+
+# Write your MySQL query statement below
+UPDATE salary
+SET 
+    sex = CASE sex
+        WHEN 'm' then 'f' 
+        WHEN 'f' then 'm'
+    END;
