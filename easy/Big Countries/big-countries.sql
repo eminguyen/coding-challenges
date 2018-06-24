f19ca3dfdf5cab65195cd7dc4b0ef534d3cb4ee4
Emily Nguyen (Antony) <emilyhuongnguyen@gmail.com>
2018-06-23 19:36:06 -0700

    Complete Big Countries in MySQL

---
+++ easy/Big Countries/big-countries.sql
@@ -0,0 +1,16 @@
+/* 
+ * Problem:
+ *
+ * There is a table World
+ * 
+ * A country is big if it has an area of bigger than 3 million square km or a population of more than 25 million.
+ * Write a SQL solution to output big countries' name, population and area.
+ */
+
+# Write your MySQL query statement below
+SELECT
+    name,population,area
+FROM 
+    World
+WHERE 
+    population > 25000000 OR area > 3000000;
