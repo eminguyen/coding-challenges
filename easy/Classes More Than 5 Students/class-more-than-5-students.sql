31f690c832b066c2677ab3ba55ed4c73ef6a7a43
Emily Nguyen (Antony) <emilyhuongnguyen@gmail.com>
2018-06-27 21:19:54 -0700

    Complete Class More Than 5 Students in MySQL

---
+++ easy/Classes More Than 5 Students/class-more-than-5-students.sql
@@ -0,0 +1,17 @@
+/*
+ * Problem:
+ *
+ * There is a table courses with columns: student and class
+ * 
+ * Please list out all classes which have more than or equal to 5 students.
+ */
+
+# Write your MySQL query statement below
+SELECT 
+    class
+FROM 
+    courses
+GROUP BY 
+    class
+HAVING 
+    COUNT(DISTINCT(student)) >= 5;
