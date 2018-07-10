b1be6e3f2c90ae1736950cccd18467cee7d1eac5
Emily Nguyen (Antony) <emilyhuongnguyen@gmail.com>
2018-07-09 23:04:07 -0700

    Complete Next Greater Element in Python3

---
+++ easy/Next Greater Element/next-greater-element.py
@@ -0,0 +1,24 @@
+"""
+You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. 
+Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+
+The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. 
+If it does not exist, output -1 for this number.
+"""
+
+class Solution:
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        greater = []
+        for i in range(len(nums1)):
+            for j in range(nums2.index(nums1[i]), len(nums2)):
+                if nums2[j] > nums1[i]:
+                    greater.append(nums2[j])
+                    break
+            else:
+                greater.append(-1)
+        return greater
