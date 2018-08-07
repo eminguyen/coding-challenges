e8896ccadc3e74d94dccd355ec0f012a06a7a229
Emily Nguyen (Antony) <emilyhuongnguyen@gmail.com>
2018-08-06 20:02:55 -0700

    Complete Solve Me First in Python3

---
+++ hacker-rank/Solve Me First/solve-me-first.py
@@ -0,0 +1,24 @@
+"""
+Problem:
+
+Complete the function solveMeFirst to compute the sum of two integers.
+
+Function prototype:
+    int solveMeFirst(int x, int y);
+        where,
+        x is the first integer input.
+        y is the second integer input
+    Return values
+        sum of the above two integers
+"""
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return a + b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
+
+
