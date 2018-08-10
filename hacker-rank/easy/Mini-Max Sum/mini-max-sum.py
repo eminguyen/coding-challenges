186d8790dd668658ed61dff1b41d1d4e35f86ce6
Emily Nguyen (Antony) <emilyhuongnguyen@gmail.com>
2018-08-09 22:11:53 -0700

    Complete Mini-Max Sum in Python3

---
+++ hacker-rank/easy/Mini-Max Sum/mini-max-sum.py
@@ -0,0 +1,26 @@
+"""
+Problem
+
+Given five positive integers, find the minimum and maximum values that can be calculated by 
+summing exactly four of the five integers. Then print the respective minimum and maximum values 
+as a single line of two space-separated long integers.
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the miniMaxSum function below.
+def miniMaxSum(arr):
+    order = sorted(arr)
+    print(sum(order[:-1]), end = " "),
+    print(sum(order[1:]))
+    
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
